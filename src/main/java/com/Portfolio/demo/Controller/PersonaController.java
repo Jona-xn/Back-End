91f2a967b055c1569d32ffdce9017d0d72e38e37
Jona-xn <95232734+Jona-xn@users.noreply.github.com>
2022-05-23 15:33:04 -0300

    En este commit hice la conexion del back con el font, agregando un link en persona.controller

--- src/main/java/com/Portfolio/demo/Controller/PersonaController.java
+++ src/main/java/com/Portfolio/demo/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.Portfolio.demo.Entity.Persona;
 import com.Portfolio.demo.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
